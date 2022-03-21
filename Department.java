43f3ee5d0efaa06884aace6055942d94afaa2ac6
kushan maduranga <48176114+kashan10@users.noreply.github.com>
2022-03-21 15:01:34 +0530

    Update Department.java
    3.01pm

--- Department.java
+++ Department.java
@@ -4,11 +4,11 @@ public class Department{
 	private String nam_of_dep;
 	private String nam_of_hed;
 	
-	public Department(int id ,String nam_of_dep,String nam_of_hed){
+	public Department(int id ,String nam_of_dep){
 		
 		this.id = id;
 		this.nam_of_dep = nam_of_dep;
-		this.nam_of_hed = nam_of_hed;
+		
 	}
 	
 	public void sethed(String nam_of_hed){
